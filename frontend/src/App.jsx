b2a2731610c1335b88681f50463434ab7fadd388
DungK5UIT <nguyenhuudung1072005@gmail.com>
2025-08-13 18:29:51 +0700

    thử tích hợp thanh toán

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Home from './pages/Home';
 import ChatBox from './components/ChatBox';
 import Catagory from './pages/Catagory';
 import Contact from './pages/Contact';
-import PaymentCallBack from './pages/PaymentCallBack';
+import PaymentCallback from './pages/PaymentCallback ';
 const App = () => {
   return (
     <AuthProvider>
@@ -27,7 +27,7 @@ const App = () => {
             <Route path="*" element={<div className="text-center p-20">404 - Trang không tồn tại</div>} />
             <Route path="/catagory" element={<Catagory />} />
             <Route path="/contact" element={<Contact />} />
-           <Route path="/payment-callback" element={<PaymentCallBack/>} />
+           <Route path="/payment-callback" element={<PaymentCallback/>} />
           </Routes>
           <ChatBox/>
         </main>
